2826db72f1abe498ae73c1b16eae57e3a4e926a2
hsclemente <54004612+hsclemente@users.noreply.github.com>
2023-07-05 23:32:25 -0300

    Update BandaDTO.java

--- src/main/java/br/com/hc/groove/bom/domain/models/dtos/BandaDTO.java
+++ src/main/java/br/com/hc/groove/bom/domain/models/dtos/BandaDTO.java
@@ -4,5 +4,5 @@ import br.com.hc.groove.bom.domain.models.entities.Banda;
 public record BandaDTO(Long id, String nome, String codigoAcesso) {
     public BandaDTO(Banda banda) {
         this(banda.getId(), banda.getNome(), banda.getCodigoAcesso());
-    } /
+    }  
 }
